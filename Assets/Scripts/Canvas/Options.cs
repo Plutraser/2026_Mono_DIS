1ee7ba41c86ee0dce05c3b18cceb5f367f3d3669
Plutraser <plutraser@gmail.com>
2026-09-22 20:10:11 -0500

    Class 9/22

--- Assets/Scripts/Canvas/Options.cs
+++ Assets/Scripts/Canvas/Options.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Canvas: Input: Music and Volume Slider, controls game options
@@ -6,6 +7,25 @@ using UnityEngine;
 
 public class Options : MonoBehaviour
 {
+    public Slider musicSlider;
+    public Slider effectsSlider;
+
+    private void Start()
+    {
+        musicSlider.value = AudioManager.Instance.volume[(int)eMixers.music];
+        effectsSlider.value = AudioManager.Instance.volume[(int)eMixers.effects];
+    }
+
+    public void OnMusicSliderChanged(float _value)
+    {
+        AudioManager.Instance.SetMixerLevel(eMixers.music, _value);
+    }
+
+    public void OnEffectsSliderChanged(float _value)
+    {
+        AudioManager.Instance.SetMixerLevel(eMixers.effects, _value);
+    }
+
     public void OnBackClicked()
     {
         Debug.Log("<color=green>OnBackClicked</color>");
--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,13 +1,27 @@
 using System;
 using UnityEngine;
+using UnityEngine.Audio;
 
 /// <summary>
 /// Manager: Handles audio in the game
 /// </summary>
+/// 
+
+public enum eMixers
+{
+    music,
+    effects
+}
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
+
+
+    public AudioMixerGroup[] mixers;
+    public float[] volume = { 1f, 1f };
+    private string[] strMixers = { "MusicVol", "EffectsVol" };
+
     public AudioSource Effects;
     public AudioClip Click;
 
@@ -15,6 +29,12 @@ public class AudioManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void SetMixerLevel (eMixers _mixer, float _soundLevel)
+    {
+        mixers[(int)_mixer].audioMixer.SetFloat(strMixers[(int)_mixer], Mathf.Log10(_soundLevel) * 20f); //db value
+        volume[(int)_mixer] = _soundLevel; // 0 to 1 value
     }
 
     public static void PlayClick()
